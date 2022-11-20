6f87a7ed6f7d229e96a78cc235360ad7825d2051
Eldar Gabdullin <eldargab@gmail.com>
2022-11-20 19:27:22 +0400

    save work

--- src/interval.rs
+++ src/interval.rs
@@ -1,1 +1,84 @@
-pub type Interval = (u32, u32);
+use std::cmp::Ordering;
+
+
+#[derive(Copy, Clone, Ord, PartialOrd, Eq, PartialEq, Hash)]
+pub struct Interval {
+    beg: u32,
+    end: u32
+}
+
+
+impl Interval {
+    pub fn new(beg: u32, end: u32) -> Self {
+        assert!(beg <= end);
+        Interval { beg, end }
+    }
+
+    #[inline]
+    pub fn begin(&self) -> u32 {
+        self.beg
+    }
+
+    #[inline]
+    pub fn end(&self) -> u32 {
+        self.end
+    }
+}
+
+
+#[derive(Clone)]
+pub struct Range {
+    intervals: Box<[Interval]>
+}
+
+
+impl Range {
+    pub fn empty() -> Self {
+        Range {
+            intervals: Box::new([])
+        }
+    }
+
+    pub fn new(intervals: Box<[Interval]>) -> Self {
+        Self::try_new(intervals).unwrap()
+    }
+
+    pub fn try_new(intervals: Box<[Interval]>) -> Option<Self> {
+        for i in 1..intervals.len() {
+            let c = intervals[i];
+            let p = intervals[i-1];
+            if p.end + 1 >= c.beg {
+                return None
+            }
+        }
+        Some(Range { intervals })
+    }
+
+    pub fn has(&self, point: u32) -> bool {
+        self.containing_interval(point).is_some()
+    }
+
+    pub fn find_containing_interval(&self, point: u32) -> Option<Interval> {
+        self.containing_interval(point).map(|i| self.intervals[i])
+    }
+
+    fn containing_interval(&self, point: u32) -> Option<usize> {
+        self.intervals.binary_search_by(|i| {
+            if point < i.beg {
+                Ordering::Greater
+            } else if i.end < point {
+                Ordering::Less
+            } else {
+                Ordering::Equal
+            }
+        }).ok()
+    }
+
+    pub fn includes(&self, interval: Interval) -> bool {
+        if let Some(c) = self.find_containing_interval(interval.beg) {
+            c.end >= interval.end
+        } else {
+            false
+        }
+    }
+}
--- src/state.rs
+++ src/state.rs
@@ -1,9 +1,10 @@
 use std::ops::Deref;
 use std::sync::{Arc, Mutex};
-use std::time::SystemTime;
+use std::time::{Duration, SystemTime};
 
-use crate::interval::Interval;
+use crate::interval::{Interval, Range};
 use crate::util::Atom;
+
 
 #[derive(Copy, Clone, Debug, Ord, PartialOrd, Eq, PartialEq)]
 pub struct WorkerId([u8; 32]);
@@ -19,15 +20,15 @@ impl From<&str> for WorkerId {
 #[derive(Clone)]
 struct Worker {
     worker_id: WorkerId,
+    desired_range: Arc<Range>,
     state: Atom<WorkerState>,
-    desired_ranges: Atom<[Interval]>
 }
 
 
 #[derive(Clone)]
 struct WorkerState {
-    url: Box<str>,
-    available_ranges: Box<[Interval]>,
+    url: Arc<str>,
+    available_range: Range,
     last_ping: SystemTime
 }
 
@@ -41,20 +42,21 @@ pub struct PingMessage {
 
 pub struct PingMessageWorkerState {
     dataset: Box<str>,
-    ranges: Box<[Interval]>
+    range: Range
 }
 
 
 pub struct PingResponse {
     dataset: Arc<str>,
-    ranges: Arc<[Interval]>
+    range: Arc<Range>
 }
 
 
 pub struct StateManager {
     dataset: Arc<str>,
-    workers: Atom<[Worker]>,
-    intervals: Mutex<Vec<Interval>>
+    workers: Atom<Vec<Worker>>,
+    intervals: Mutex<Vec<Interval>>,
+    replication: usize
 }
 
 
@@ -63,42 +65,102 @@ unsafe impl Sync for StateManager {}
 
 
 impl StateManager {
+    pub fn get_worker(&self, first_block: u32) -> Option<Arc<str>> {
+        let now = SystemTime::now();
+
+        let candidates: Vec<_> = self.workers.get()
+            .iter()
+            .filter_map(|w| {
+                let state = w.state.get();
+                let since_last_ping = now.duration_since(state.last_ping).unwrap_or(Duration::from_secs(0));
+
+                let is_suitable = since_last_ping < Duration::from_secs(30)
+                    && state.available_range.has(first_block)
+                    && w.desired_range.has(first_block);
+
+                if is_suitable {
+                    Some(state.url.clone())
+                } else {
+                    None
+                }
+            }).collect();
+
+        if candidates.len() == 0 {
+            None
+        } else {
+            let i: usize = rand::random();
+            Some(candidates[i % candidates.len()].clone())
+        }
+    }
+
     pub fn ping(&self, msg: PingMessage) -> PingResponse {
         let worker_id = WorkerId::from(msg.worker_id.deref());
 
         let state = Arc::new(WorkerState {
-            url: msg.worker_url,
-            available_ranges: match msg.worker_state {
-                Some(s) if s.dataset.deref() == self.dataset.deref() => s.ranges,
-                _ => Box::new([])
+            url: msg.worker_url.into(),
+            available_range: match msg.worker_state {
+                Some(s) if s.dataset.deref() == self.dataset.deref() => s.range,
+                _ => Range::empty()
             },
             last_ping: SystemTime::now()
         });
 
-        let mut desired_ranges: Option<Arc<[Interval]>> = None;
+        let mut desired_range: Option<Arc<Range>> = None;
 
         self.workers.update(|workers| {
             if let Some(w) = workers.iter().find(|w| w.worker_id == worker_id) {
                 w.state.set(state.clone());
-                desired_ranges = Some(w.desired_ranges.get());
+                desired_range = Some(w.desired_range.clone());
                 None
             } else {
-                let ranges = Arc::from_iter(state.available_ranges.iter().cloned());
-                desired_ranges = Some(ranges.clone());
+                let range = Arc::new(state.available_range.clone());
+                desired_range = Some(range.clone());
                 let w = Worker {
                     worker_id,
                     state: Atom::new(state.clone()),
-                    desired_ranges: Atom::new(ranges)
+                    desired_range: range
                 };
-                Some(Arc::from_iter(
-                    workers.iter().cloned().chain(std::iter::once(w))
+                Some(Arc::new(
+                    workers.iter().cloned().chain(std::iter::once(w)).collect()
                 ))
             }
         });
 
         PingResponse {
             dataset: self.dataset.clone(),
-            ranges: desired_ranges.unwrap()
+            range: desired_range.unwrap()
+        }
+    }
+
+    pub fn schedule(&self) {
+        let now = SystemTime::now();
+        let lock = self.intervals.lock().unwrap();
+        let intervals = lock.deref();
+        let mut workers = self.workers.get().deref().clone();
+
+        // remove dead workers
+        workers.retain(|w| {
+            let since_last_ping = now.duration_since(w.state.get().last_ping).unwrap_or(Duration::from_secs(0));
+            since_last_ping < Duration::from_secs(10 * 60)
+        });
+
+        let mut desired_ranges = workers
+            .iter()
+            .map(|w| w.desired_range.deref().clone())
+            .collect::<Vec<_>>();
+
+        for &interval in intervals.iter() {
+            let holders: Vec<usize> = desired_ranges
+                .iter()
+                .enumerate()
+                .filter_map(|(idx, r)| {
+                    if r.has(interval.begin()) {
+                        Some(idx)
+                    } else {
+                        None
+                    }
+                })
+                .collect();
         }
     }
 }
